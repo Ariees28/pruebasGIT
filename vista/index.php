6360288a6e3ae21b412e357ee6136861d72a6746
Jorge Rodriguez <sirius-black24@hotmail.com>
2021-09-01 01:04:02 -0500

    se agrego una carpeta y un archivo css, se modifico el index para que tuviera una mejor vista y se agrego un .gitignore

--- vista/index.php
+++ vista/index.php
@@ -4,24 +4,38 @@ $con = conectar();
 ?>
 
 <!<!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
 	<meta charset="utf-8">
 	<title>Pruebas</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
-	<div>
-		<h1>Pagina de pruebas</h1>
-		<h2>Esta es la pagina index en la que se mostraran todas las pruebas que se realizan con los lenguajes de HTML, PHP, mysqli, JavaScript y css</h2>
-		<p>En teoria, esta pagina se va a ir actualizando para que se vea mas bonita, a medida que voy aprendiendo de html, css y js</p>
-	</div>
-	<br><br>
-	<div style="color: blue; border: solid       red 1px;">
-		<h2>Pruebas de conexion a base de datos</h2>
-		<span>texto prueba</span>
-		<p>Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Ducimus, voluptas eum laboriosam numquam veniam nulla, nostrum odio aut illum pariatur, sequi vel consequatur dicta. Ea eligendi, nihil? Culpa, mollitia animi.</p>
-		<div style="border: solid silver 5px;">
-			<p>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Distinctio necessitatibus dolores mollitia excepturi aspernatur ullam modi omnis unde ducimus sint, animi voluptates sapiente illum laudantium, nobis doloremque magnam dolor quo.</p>
+	<div id="contenedor">
+		<div id="header" class="contenedores">
+			<h1>Pagina de pruebas</h1>
+			<br>
+			<h2>Esta es la pagina index en la que se mostraran todas las pruebas que se realizan con los lenguajes de HTML, PHP, mysqli, JavaScript y css</h2>
+			<br>
+			<p>En teoria, esta pagina se va a ir actualizando para que se vea mas bonita, a medida que voy aprendiendo de html, css y js</p>
+		</div>
+		<div id="segundoContenedor" class="contenedores">
+			<h2>Pruebas de conexion a base de datos</h2>
+			<div id="tabla">
+				<br>
+				<h4>PRUEBA TABLA</h4><br>
+				<p>Esta prueba fue para mostrar una tabla traida de una base de datos mediante php.</p><br>
+				<button id="btnTabla">MOSTRAR TABLA</button>
+			</div>
+			<div id="crud">
+				<br>
+				<h4>PRUEBA CRUD</h4><br>
+				<p>Esta prueba fue para crear un crud que pueda llenar campos de una base de datos, mediante php</p><br>
+				<button id="btnCrud">MOSTRAR CRUD</button>
+			</div>
+		</div>
+		<div id="otras" class="contenedores">
+			<h2>OTRAS PRUEBAS</h2>
 		</div>
 	</div>
 </body>
---
+++ vista/tabla.php
@@ -0,0 +1,11 @@
+<!<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PRUEBA DE CONEXION</title>
+</head>
+<body>
+	<h1>PRUEBA DE CONEXION A BASE DE DATOS</h1>
+	
+</body>
+</html>
